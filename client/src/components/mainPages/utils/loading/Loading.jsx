3bf341577a506fd4f7ffc0e39259fb72429eed21
pelumi <you@example.com>
2020-12-06 11:11:53 -0800

    make it better

---
+++ client/src/components/mainPages/utils/loading/Loading.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function Loading() {
+    return (
+        <div className="load-page">
+            <div className="loader">
+                <div>
+                    <div>
+                        <div>
+                            <div>
+                                <div>
+                                    <div>
+                                        <div>
+                                            <div></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
